16a050e156bd7d33c2a173465b21b273c77efc3b
chainreaction6 <blora616@gmail.com>
2022-07-03 22:28:41 -0400

    restarting

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+const hre = require("hardhat");
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,17 +14,19 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Vault = await ethers.getContractFactory("Vault");
-  const vault = await Vault.deploy("Hello, Hardhat!");
+  const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  await vault.deployed();
+  await greeter.deployed();
 
-  console.log("Greeter deployed to:", vault.address);
+  console.log("Greeter deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
