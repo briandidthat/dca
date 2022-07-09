25375a080480322b697e8a88becadab33b783fea
chainreaction6 <blora616@gmail.com>
2022-07-08 22:53:30 -0400

    test swap functionality

--- test/uniswapExchange.test.js
+++ test/uniswapExchange.test.js
@@ -7,9 +7,10 @@ const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 const USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7";
 
 const WHALE = "0x7a8edc710ddeadddb0b539de83f3a306a621e823";
+const USDT_WHALE = "0xa929022c9107643515f5c777ce9a910f0d1e490c";
 
 describe("UniswapExchange", async () => {
-  let accounts, dev, whale;
+  let accounts, dev, whale, usdtWhale;
   let exchange;
   let weth, dai, usdt, usdc;
   const daiAmount = 100n * 10n ** 18n;
@@ -17,7 +18,7 @@ describe("UniswapExchange", async () => {
   const usdtAmount = 100n * 10n ** 6n;
 
   beforeEach(async () => {
-    [dev, ...accounts] = await ethers.getSigners();
+    [dev, user, ...accounts] = await ethers.getSigners();
     const Exchange = await ethers.getContractFactory("UniswapExchange", dev);
     exchange = await Exchange.deploy();
     await exchange.deployed();
@@ -33,16 +34,18 @@ describe("UniswapExchange", async () => {
       params: [WHALE],
     });
 
-    whale = await ethers.getSigner(WHALE);
-    // transfer 100 USDC & 100 DAI from whale to dev
-    await dai.connect(whale).transfer(dev.address, daiAmount);
-    await usdc.connect(whale).transfer(dev.address, usdcAmount);
-    
-  });
+    // unlock USDT Whale account
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [USDT_WHALE],
+    });
 
-  it("Should give the dev address 100 USDC & 100 DAI to trade", async () => {
-    expect(await dai.balanceOf(dev.address)).to.gte(daiAmount);
-    expect(await usdc.balanceOf(dev.address)).to.gte(usdcAmount);
+    whale = await ethers.getSigner(WHALE);
+    usdtWhale = await ethers.getSigner(USDT_WHALE);
+    // transfer 100 USDC & 100 DAI from whale to dev
+    await dai.connect(whale).transfer(user.address, daiAmount);
+    await usdc.connect(whale).transfer(user.address, usdcAmount);
+    await usdt.connect(usdtWhale).transfer(user.address, usdtAmount);
   });
 
   it("Should deploy contract with dev as owner", async () => {
@@ -50,27 +53,32 @@ describe("UniswapExchange", async () => {
     expect(owner).to.equal(dev.address);
   });
 
-  it("swapExactInputSingle: Should swap 100 DAI for WETH", async () => {
-    const balanceBefore = await weth.balanceOf(dev.address);
-    await dai.connect(dev).approve(exchange.address, daiAmount);
-    await exchange.connect(dev).swapExactInputSingle(daiAmount, DAI);
+  it("Should give the user address 100 USDC & 100 DAI to trade", async () => {
+    expect(await dai.balanceOf(user.address)).to.gte(daiAmount);
+    expect(await usdc.balanceOf(user.address)).to.gte(usdcAmount);
+  });
 
-    expect(await weth.balanceOf(dev.address)).to.gte(balanceBefore);
+  it("swapExactInputSingle: Should swap 100 DAI for WETH", async () => {
+    const balanceBefore = await weth.balanceOf(user.address);
+    await dai.connect(user).approve(exchange.address, daiAmount);
+    await exchange.connect(user).swapExactInputSingle(daiAmount, DAI);
+
+    expect(await weth.balanceOf(user.address)).to.gte(balanceBefore);
   });
 
   it("swapExactInputSingle: Should swap 100 USDC for WETH", async () => {
-    const balanceBefore = await weth.balanceOf(dev.address);
-    await usdc.connect(dev).approve(exchange.address, usdcAmount);
-    await exchange.connect(dev).swapExactInputSingle(usdcAmount, USDC);
+    const balanceBefore = await weth.balanceOf(user.address);
+    await usdc.connect(user).approve(exchange.address, usdcAmount);
+    await exchange.connect(user).swapExactInputSingle(usdcAmount, USDC);
 
-    expect(await weth.balanceOf(dev.address)).to.gte(balanceBefore);
+    expect(await weth.balanceOf(user.address)).to.gte(balanceBefore);
   });
 
   it("swapExactInputSingle: Should swap 100 USDT for WETH", async () => {
-    const balanceBefore = await weth.balanceOf(dev.address);
-    await usdt.connect(dev).approve(exchange.address, usdtAmount);
-    await exchange.connect(dev).swapExactInputSingle(usdtAmount, USDT);
+    const balanceBefore = await weth.balanceOf(user.address);
+    await usdt.connect(user).approve(exchange.address, usdtAmount);
+    await exchange.connect(user).swapExactInputSingle(usdtAmount, USDT);
 
-    expect(await weth.balanceOf(dev.address)).to.gte(balanceBefore);
+    expect(await weth.balanceOf(user.address)).to.gte(balanceBefore);
   });
 });
