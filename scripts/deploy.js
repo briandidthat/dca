4b0a4a938d3d8ccc68766ff48c226a9abeb5cc6f
chainreaction6 <blora616@gmail.com>
2022-09-06 23:07:46 -0400

    fix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
-  const [account, user] = await hre.ethers.getSigners();
+  const [dev, user] = await hre.ethers.getSigners();
 
   const Library = await ethers.getContractFactory("TokenLibrary");
   const library = await Library.deploy();
@@ -18,28 +18,26 @@ async function main() {
   });
 
   const chamberFactory = await ChamberFactory.deploy();
-
   await chamberFactory.deployed();
 
-  console.log("ChamberFactory deployed. Owner: " + account.address);
+  console.log("ChamberFactory deployed. Owner: " + dev.address);
 
   const owner = await chamberFactory.owner();
-  assert(owner === account.address);
+  assert(owner === dev.address);
 
   let chamberDeployment = await chamberFactory.connect(user).deployChamber({
     value: hre.ethers.utils.parseEther("0.05"),
   });
 
-  chamberDeployment = await chamberDeployment.wait()
+  chamberDeployment = await chamberDeployment.wait();
   const chamberAddr = chamberDeployment.events[1].args[0];
 
   console.log("Chamber deployed. Address: " + chamberAddr);
 
   const chamber = await hre.ethers.getContractAt("Chamber", chamberAddr);
-  
+
   const chamberOwner = await chamber.getOwner();
   assert(chamberOwner === user.address);
-
 }
 
 main().catch((error) => {
